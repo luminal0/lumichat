e11ba5156386620420567eb2a202d2a69562dfc6
Satrio Coman Azizi <luminalwww@gmail.com>
2024-03-09 23:43:39 +0700

    routes and models

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,23 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/message.routes.js";
+
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 const __dirname = path.resolve();
 
-dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
