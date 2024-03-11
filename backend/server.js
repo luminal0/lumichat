6a14673dfdb6060f70c81114b604364c9153cdfe
Satrio Coman Azizi <luminalwww@gmail.com>
2024-03-11 20:03:28 +0700

    added message and user controller/model

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-import authRoutes from "./routes/message.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -20,6 +22,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
