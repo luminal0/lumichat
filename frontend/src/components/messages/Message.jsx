4f7cc398fee79a67d68ae85004a6563738f90079
Satrio Coman Azizi <luminalwww@gmail.com>
2024-03-19 03:45:39 +0700

    finished, more improvement to be done

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,26 @@
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../extractTime";
 
-const Message = () => {
+const Message = ({message }) => {
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useConversation();
+  const formattedTime = extractTime(message.createdAt);
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500' : 'bg-grey-500';
+  
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName} `}>
         <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-                <img src="https://avatar.iran.liara.run/public/13" alt="Component" />
+                <img src={profilePic} alt="Component" />
 
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hello</div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   )
 }
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-  return (
-    <form className="flex items-center gap-2">
-        <input type="text" placeholder="Search" className="input input-bordered rounded-full" />
-        <button type="submit" className="btn btn-circle bg-sky-500 text-white">
-        <FaSearch className="w-5 h-5 outline-none" />
-        </button>
-    </form>
-  )
-}
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
 
-export default SearchInput
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search must be at least 3 characters");
+  };
+
+  const conversation = conversations.find((conversation) =>
+    conversation.fullName.toLowerCase().includes(search.toLowerCase())
+  );
+  if (conversation) {
+    setSelectedConversation(conversation);
+    setSearch("");
+  } else toast.error("No conversation found");
+
+};
+  return (
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Search"
+        className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <FaSearch className="w-5 h-5 outline-none" />
+      </button>
+    </form>
+  );
+};
+
+export default SearchInput;
